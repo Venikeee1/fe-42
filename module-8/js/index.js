207a4bcb9ffde95a96cc468bbfebf48a9d685cc0
oleksandr_vienik <alexandrVenik@gmail.com>
2022-01-24 22:18:23 +0200

    add module 7 lesson 1

---
+++ module-8/js/index.js
@@ -0,0 +1,13 @@
+import { User as SuperUser } from './user';
+import superFunction from './user';
+import '../css/style.css';
+
+const fooRef = document.querySelector('#foo');
+const barRef = document.querySelector('#bar');
+
+console.log(fooRef, barRef);
+
+const user = new SuperUser();
+user.say();
+
+superFunction('dsadasdasdas');
---
+++ module-8/js/user.js
@@ -0,0 +1,9 @@
+export class User {
+  say() {
+    console.log('hello');
+  }
+}
+
+export default (val) => {
+  console.log(val);
+};
